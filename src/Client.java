f6c562a93f67ea9012684628717a898728c8910d
karenlstengel <#Justdance1>
2018-09-19 11:24:36 -0600

    print statements for mathematica in client

--- src/Client.java
+++ src/Client.java
@@ -5,22 +5,22 @@ public class Client {
         int[] testArr = {10, 3, 5, 7, 8, 2, 4, 7, 9, 11, 12, 1};
 
 
-        //client creations
+        //Mathematica creations
         Mathematica mathematica = new Mathematica();
 
         //print out test array
         System.out.println("Random array: ");
         mathematica.ms.printArray(testArr);
 
-        //client running
+        //Mathematica running on default
         int[] mathematicaArr = mathematica.ms.mathSort(testArr);
         mathematica.ms.printArray(mathematicaArr);
 
-        //dynamic client
+        //dynamic Mathematica
         mathematica.setSortStrategy(new BubbleSort());
 
         //print out test array
-        System.out.println("Random array: ");
+        System.out.println("\nRandom array: ");
         mathematica.ms.printArray(testArr);
 
         mathematicaArr = mathematica.ms.mathSort(testArr);

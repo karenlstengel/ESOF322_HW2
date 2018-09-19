ca3085a01bcf2cb732fc85ff74afea7e0a2cdc38
karenlstengel <#Justdance1>
2018-09-19 10:44:22 -0600

    added extra value to array

--- src/Client.java
+++ src/Client.java
@@ -2,7 +2,7 @@ public class Client {
     //main method
     public static void main(String args[]) {
 
-        int[] testArr = {10, 3, 5, 7, 8, 2, 4, 7, 9, 11, 12};
+        int[] testArr = {10, 3, 5, 7, 8, 2, 4, 7, 9, 11, 12, 1};
 
 
 
